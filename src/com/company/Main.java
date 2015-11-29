1223132653db6bfd055c5eb6a4bacfa31835f994
tiaotiao <tiaotiaoyly@gmail.com>
2015-11-29 13:39:35 -0600

    fix server config

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length <= 2) {
+        if (args.length < 2) {
             printHelp();
             return;
         }
