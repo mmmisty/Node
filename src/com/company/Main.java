b4b78308d0faf0d28f2e78e25040862e2ad412c0
tiaotiao <tiaotiaoyly@gmail.com>
2015-11-29 13:38:10 -0600

    fix server config

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length <= 0) {
+        if (args.length <= 2) {
             printHelp();
             return;
         }
 
         String cmd = args[0];
+        String param = args[1];
+
         if (cmd.equals("server")) {
             // Node server
-            NodeServer server1 = new NodeServer(args[0]);
+            NodeServer server1 = new NodeServer(param);
             server1.Start();
             // Node Client
             Commander c1 = new Commander();
@@ -19,23 +21,23 @@ public class Main {
 
         } else if (cmd.equals("test")) {
             TestCase t = new TestCase();
-            t.Run(args[1]);
+            t.Run(param);
 
         } else if (cmd.equals("redis")) {
             ConnectRedis r = new ConnectRedis();
-            r.Run(args[1]);
+            r.Run(param);
 
         } else if (cmd.equals("mongodb")) {
             ConnectMongoDB m = new ConnectMongoDB();
-            m.Run(args[1]);
+            m.Run(param);
 
         } else if (cmd.equals("riak")) {
             ConnectRiak r2 = new ConnectRiak();
-            r2.Run(args[1]);
+            r2.Run(param);
 
         } else if (cmd.equals("hbase")) {
             ConnectHBase h = new ConnectHBase();
-            h.Run(args[1]);
+            h.Run(param);
         }
     }
 
